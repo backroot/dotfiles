c3d8c80810579a37b5e265f4aae7f3c6cae4e5ed
backroot <kotaro@codebase.jp>
2017-05-14 01:02:21 -0400

    add Vundle and Jedi

--- setup_vim.sh
+++ setup_vim.sh
@@ -15,6 +15,10 @@ sudo yum -y install git
 mkdir -p ~/.vim/dein/repos/github.com/Shougo/dein.vim
 git clone https://github.com/Shougo/dein.vim.git ~/.vim/dein/repos/github.com/Shougo/dein.vim
 
+# install vundle
+mkdir -p ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 # link to ~/.vimrc
 SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 ln -s $SCRIPT_DIR/.vimrc ~/.vimrc
